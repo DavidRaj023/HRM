de2fbce0188a88f264e79ad54c1a1c2246a36a4e
DavidRaj023 <benjamindavid023@gmail.com>
2021-12-02 17:34:21 +0530

    Completed

--- src/middleware/emp-route.js
+++ src/middleware/emp-route.js
@@ -2,8 +2,8 @@ const express = require('express');
 const Employee = require('../model/employee');
 const router = new express.Router();
 
-//Create employees
-router.post('/api/v1/employee', async (req, res) =>{
+//DAO
+const createEmployee = async (req, res) =>{
     const emp = new Employee(req.body);
     try {
         await emp.save();
@@ -11,55 +11,18 @@ router.post('/api/v1/employee', async (req, res) =>{
     } catch (e) {
         res.status(400).send(e);
     }
-})
+}
 
-//Get Employees
-router.get('/api/v1/employees', async (req, res) => {
+const getAllEmployees = async (req, res) => {
     try {
         const emp = await Employee.find({});
         res.send(emp);
     } catch (e) {
         res.status(500).send(e);
     }
-})
+}
 
-//Get Employees by fillter
-router.post('/api/v1/employees', async (req, res) => {
-    const filter = req.body.filter;
-    if(filter === 'all'){
-        try {
-            console.log(filter);
-            const emp = await Employee.find({});
-            res.send(emp);
-        } catch (error) {
-            res.status(500).send(e);
-        }    
-    } else if(filter === 'gender'){
-        try {
-            const value = req.body.value
-            console.log('Filter: ' + filter);
-            console.log('Value: ' + value);
-            const emp = await Employee.find({ gender: value });
-            res.send(emp);
-        } catch (error) {
-            res.status(500).send(e);
-        }    
-    } else if(filter === 'department'){
-        try {
-            const value = req.body.value
-            console.log('Filter: ' + filter);
-            console.log('Value: ' + value);
-            const emp = await Employee.find({ department: value });
-            res.send(emp);
-        } catch (error) {
-            res.status(500).send(e);
-        }    
-    }
-})
-
-
-// //Update Task
-router.post('/api/v1/edit-employee', async (req, res) => {
+const updateEmployee = async (req, res) => {
     const id = req.body.empId;
     console.log(id);
     const updates = ['name', 'phone', 'age'];
@@ -79,14 +42,13 @@ router.post('/api/v1/edit-employee', async (req, res) => {
     } catch (e) {
         res.status(500).send(e);
     }
-})
+}
 
-//Delete User
-router.post('/api/v1/remove-employee', async (req, res) => {
+const deleteEmployee = async (req, res) => {
     const id = req.body.empId;
     console.log(id);
     try {
-        const emp = await Employee.findOneAndDelete(id);
+        const emp = await Employee.findOneAndDelete( {empId: id} );
         console.log(emp);
         if(!emp){
             return res.status(404).send();
@@ -95,7 +57,107 @@ router.post('/api/v1/remove-employee', async (req, res) => {
     } catch (e) {
         res.status(500).send(e);
     }
-})
+}
 
+const getByFilters = async (req, res) => {
+    const filter = req.body.filter;
+    switch (filter) {
+        case 'all':
+            try {
+                console.log('Filter: ' + filter);
+                const emp = await Employee.find({});
+                console.log(emp.length);
+                res.send(emp);
+            } catch (e) {
+                res.status(500).send(e);
+            }
+            break;
+
+        case 'gender':
+            try {
+                const value = req.body.value;
+                console.log('Filter: ' + filter);
+                console.log('Value: ' + value);
+                const emp = await Employee.find({ gender: value });
+                res.send(emp);
+            } catch (e) {
+                res.status(500).send(e);
+            }
+            break;
+
+        case 'department':
+            try {
+                const value = req.body.value;
+                console.log('Filter: ' + filter);
+                console.log('Value: ' + value);
+                const emp = await Employee.find({ department: value });
+                res.send(emp);
+            } catch (e) {
+                res.status(500).send(e);
+            }
+            break;
+
+        case 'age':
+            try {
+                const option = req.body.option; 
+                const value = req.body.value;
+                console.log('Filter: ' + filter + ' Option: ' + option + ' Value: ' + value);
+                if(option === 'less_than'){
+                    const emp = await Employee.find({ age: { $lt: value } });
+                    return res.send(emp);
+                }
+                const emp = await Employee.find({ age: { $gt: value } });
+                res.send(emp);
+            } catch (e) {
+                res.status(500).send(e);
+            }
+            break;
+
+        case 'salary':
+            try {
+                const option = req.body.option; 
+                const value = req.body.value;
+                console.log('Filter: ' + filter + ' Option: ' + option + ' Value: ' + value);
+                if(option === 'less_than'){
+                    const emp = await Employee.find({ salary: { $lt: value } });
+                    return res.send(emp);
+                }
+                const emp = await Employee.find({ salary: { $gt: value } });
+                res.send(emp);
+            } catch (e) {
+                res.status(500).send(e);
+            }
+            break;
+
+        case 'doj':
+            try {
+                const option = req.body.option; 
+                const value = req.body.value;
+                console.log('Filter: ' + filter + ' Option: ' + option + ' Value: ' + value);
+                if(option === 'before'){
+                    const emp = await Employee.find({ doj: { $lt: value } });
+                    return res.send(emp);
+                }
+                const emp = await Employee.find({ doj: { $gt: value } });
+                res.send(emp);
+            } catch (e) {
+                res.status(500).send(e);
+            }
+            break;
+
+        default:
+            res.status(400).send({
+                "Message": "This filter is not available"
+            })
+            break;
+    }
+}
+
+//Routers
+router.post('/api/v1/employee',createEmployee);
+router.get('/api/v1/employees', getAllEmployees);
+router.post('/api/v1/edit-employee', updateEmployee);
+router.post('/api/v1/remove-employee', deleteEmployee);
+router.post('/api/v1/employees', getByFilters);
 
 module.exports = router;
